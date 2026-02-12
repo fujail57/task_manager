777e3c93ea6cf401012cd2f35d80cf601aa14a90
Fujail Akram <fujail5july@gmail.com>
2026-02-12 21:35:07 +0530

    About page make responsivee

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,20 +1,3 @@
-// import React from "react";
-
-// export const About = () => {
-//   return (
-//     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50  space-y-5">
-//       <h1 className="font-black text-4xl">Task Manager</h1>
-//       <h3 className="font-semibold text-2xl">Stay organized. Stay productive.</h3>
-//       <p className="w-2xl mx-auto">
-//         Keep track of your daily tasks, set priorities, and never miss a
-//         deadline. Manage your time efficiently and turn your to-do list into
-//         done.
-//       </p>
-//     </div>
-//   );
-// };
-
-
 import React from "react";
 
 export const About = () => {
